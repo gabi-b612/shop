80bb31e1254d04fa2821e5f1e66a076a424ff21f
gabi-b612 <20km095@esisalama.org>
2022-10-24 21:47:05 +0200

    fonctions add and show product

--- config/command.php
+++ config/command.php
@@ -1,1 +1,20 @@
 <?php
+
+function add($name, $picture, $price, $desc) {
+    if (require ("connection.php")) {
+        $req = $access -> prepare ("INSERT INTO produits (image, nom, prix, description) values ($picture, $name, $price, $desc)");
+        $req -> execute(array($name, $picture, $price, $desc));
+        $req -> closeCursor();
+    }
+}
+
+function show () {
+    if (require ("connection.php")) {
+        $req = $access -> prepare ("SELECT * FROM produits ");
+        $req -> execute();
+        $data = $req -> fetchAll(PDO::FETCH_OBJ);
+        return $data;
+        $req -> closeCursor();
+    }
+}
+?>
