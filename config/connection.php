6977a0f3aaa2002adb7b2f1b0ebd6f7e9f9bc7be
gabi-b612 <20km095@esisalama.org>
2022-10-24 20:57:59 +0200

    connexion de la database

--- config/connection.php
+++ config/connection.php
@@ -1,1 +1,11 @@
 <?php
+
+try {
+    $access = new PDO("mysql:host=localhost;dbname=dolcavita;charset=utf8", "carene", "carene");
+
+    $access -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (Exception $e) {
+    $e -> getMessage();
+}
+
+?>
