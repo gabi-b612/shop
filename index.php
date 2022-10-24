616d154311b8e594f938ca2b247edfb754533cb2
gabi-b612 <20km095@esisalama.org>
2022-10-24 22:35:50 +0200

    include balises php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require ("config/command.php");
-    $mesProduits = show();
+    $product = show();
 ?>
 
 <!doctype html>
@@ -77,24 +77,23 @@
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-
+            <?php  foreach ($product as $oneproduct): ?>
                 <div class="col">
                     <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title><?= $oneproduct -> nom ?></title><rect width="100%" height="100%" fill="#55595c"/> <img src="<?php $oneproduct -> image  ?>" </svg>
 
                         <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><? $oneproduct -> desc ?></p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Afficher</button>
                                 </div>
-                                <small class="text-muted">9 mins</small>
+                                <small class="text-muted"><? $oneproduct -> prix ?></small>
                             </div>
                         </div>
                     </div>
                 </div>
-
+            <?php  endforeach;?>
 
             </div>
         </div>
